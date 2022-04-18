7c7bf8dc73b6ef626467bf75b8c570693cd531cc
Joshua Correa <jscorrea1@buffs.wtamu.edu>
2022-04-17 22:33:26 -0500

    database.py

--- pestslib/database.py
+++ pestslib/database.py
@@ -1,7 +1,6 @@
 from sqlite3 import IntegrityError
 import string
-from tkinter.tix import COLUMN, INTEGER
-from tokenize import String
+from tkinter.tix import INTEGER
 from xmlrpc.client import DateTime
 import sqlalchemy as db
 from pests import Pest, PestObservation, Illness, DiagnosticTesting
@@ -9,14 +8,14 @@ from pests import Pest, PestObservation, Illness, DiagnosticTesting
 Pests = db.Table(
     'Pest',
     db.Column('Id', INTEGER, primary_key=True),
-    db.Column('genus', String),
-    db.Column('species', String)
+    db.Column('genus', string),
+    db.Column('species', string)
 )
 
 PestObservations = db.Table(
     'PestObservation',
     db.Column('Id', INTEGER, primary_key=True),
-    db.Column('city', String),
-    db.Column('county', String),
+    db.Column('city', string),
+    db.Column('county', string),
     db.Column('obs_date', DateTime)
 )
